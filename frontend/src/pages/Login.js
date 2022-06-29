ea1e76e89accc854a86a51bf76e7cc684b94875a
SHIKI65536 <shiki65536@gmail.com>
2022-06-29 09:42:33 +0800

    add sample account

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -62,7 +62,6 @@ function Login() {
           <FaSignInAlt /> Login
         </h1>
         <p>Login and start setting goals</p>
-        <p>sample account: guest@gmail.com/123456</p>
       </section>
 
       <section className='form'>
@@ -96,6 +95,7 @@ function Login() {
             </button>
           </div>
         </form>
+        <p>sample account: guest@gmail.com/123456</p>
       </section>
     </>
   )
