cb7e6a79bbf4f735d57b1ddeac51d895aba27bc6
shiki65536shiki65536 <shiki65535@dyn-118-138-126-158.its.monash.edu.au>
2023-02-28 12:00:04 +1100

    move to render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb+srv://shiki:65536@cluster0.pruopa2.mongodb.net/?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.log(`Error: ${err.message}`);
