dc218042731568dc0782944df2e4838521073b99
shiki65536shiki65536 <shiki65535@dyn-118-138-126-158.its.monash.edu.au>
2023-02-28 13:32:39 +1100

    amend mongo setting

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: "true",});
+    const conn = await mongoose.connect('mongodb+srv://shiki:65536@cluster0.pruopa2.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: "true",useUnifiedTopology: "true"});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.log(`Error: ${err.message}`);
