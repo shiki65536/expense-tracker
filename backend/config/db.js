fc2da7a97dedcf6e2b1aef8c4e67d1490bfbb6d6
shiki65536shiki65536 <shiki65535@dyn-118-138-126-158.its.monash.edu.au>
2023-02-28 13:28:52 +1100

    amend mongo setting

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: "true",});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.log(`Error: ${err.message}`);
